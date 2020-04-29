fec4504a2c77895ba208d3fac65ded6e3a5263e2
luwei <1280380446@qq.com>
2020-04-29 18:15:50 +0800

    反转链表-尾递归法

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -18,17 +18,33 @@
  */
 
 // 迭代法
+// var reverseList = function (head) {
+//   let prev = null;
+//   let current = head;
+//   while (current) {
+//     let next = current.next;
+//     current.next = prev;
+//     prev = current;
+//     current = next;
+//     // 可以简写成
+//     // [current.next, current, prev] = [prev, current.next, current];
+//   }
+//   return prev;
+// };
+
+// 尾递归法
 var reverseList = function (head) {
+  if (head === null || head.next === null) return head;
   let prev = null;
-  let current = head;
-  while (current) {
-    let next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-    // 可以简写成
-    // [current.next, current, prev] = [prev, current.next, current];
-  }
-  return prev;
+  const reverse = function (first, second) {
+    let next = second.next;
+    second.next = first;
+    if (next === null) {
+      return second;
+    } else {
+      return reverse(second, next);
+    }
+  };
+  return reverse(prev, head);
 };
 // @lc code=end
