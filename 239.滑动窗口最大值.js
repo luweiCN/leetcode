10999c868ea0325fa0906ae945fc07b181bd38d9
wei.lu <wei.lu@aihuishou.com>
2020-05-08 17:14:28 +0800

    239.滑动窗口最大值

--- 239.滑动窗口最大值.js
+++ 239.滑动窗口最大值.js
@@ -10,28 +10,48 @@
  * @param {number} k
  * @return {number[]}
  */
+
+// 暴力解法
+// var maxSlidingWindow = function (nums, k) {
+//   if (nums.length <= 0) return [];
+//   if (k == 1) return nums;
+//   let current = [];
+//   let max_arr = [];
+//   let i = 0;
+//   let j = 0;
+//   while (j <= nums.length) {
+//     if (j > k - 1) {
+//       max_arr.push(Math.max(...current));
+//       current.push(nums[j]);
+//       current.shift(nums[i]);
+//       i++;
+//       j++;
+//     } else {
+//       current.push(nums[j]);
+//       j++;
+//     }
+//   }
+
+//   return max_arr;
+// };
+
+// 双端队列
 var maxSlidingWindow = function (nums, k) {
-  if (nums.length <= 0) return [];
-  if (k == 1) return nums;
-  let current = [];
+  let deque = [];
   let max_arr = [];
-  let i = 0;
-  let j = 0;
-  while (j <= nums.length) {
-    if (j > k - 1) {
-      max_arr.push(Math.max(...current));
-      current.push(nums[j]);
-      current.shift(nums[i]);
-      i++;
-      j++;
-    } else {
-      current.push(nums[j]);
-      j++;
+  for (let i = 0; i < nums.length; i++) {
+    while (nums[deque[deque.length - 1]] < nums[i]) {
+      deque.pop();
+    }
+    deque.push(i);
+    if (i - deque[0] >= k) {
+      deque.shift();
+    }
+    if (i >= k - 1) {
+      max_arr.push(nums[deque[0]]);
     }
   }
-
   return max_arr;
 };
 
-// console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
 // @lc code=end
