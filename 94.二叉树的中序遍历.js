d20b71a43c6bacd04c55831139c2357c605298fb
wei.lu <wei.lu@aihuishou.com>
2020-05-12 17:58:54 +0800

    94.二叉树的中序遍历

--- 94.二叉树的中序遍历.js
+++ 94.二叉树的中序遍历.js
@@ -30,15 +30,17 @@
 // 迭代法
 var inorderTraversal = function (root) {
   if (root === null) return [];
-  let stack = [root];
+  let stack = [root]; // 利用栈来遍历树
   let res = [];
   while (stack.length) {
     let current = stack.pop();
     if (current === null) continue;
     if (!current.visited) {
-      current.visited = true;
-      stack.push(current.right, current, current.left);
+      // 节点未被访问
+      current.visited = true; // 设置了一个变量，标记该节点是否被访问了
+      stack.push(current.right, current, current.left); // 不管三七二十一，按照右根左的顺序全部入栈，即使有null的也会在上面continue的时候跳过
     } else {
+      // 节点已经被访问了，输出值，遍历栈里的下一个节点
       res.push(current.val);
     }
   }
